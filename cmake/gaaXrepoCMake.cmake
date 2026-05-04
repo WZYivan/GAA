8316809f524ccbd6f71c72ab812cd640cd6c0f19
WZYivan <2270060975@qq.com>
2026-05-04 13:26:26 +0800

    [build] local xrepo.cmake

--- cmake/gaaXrepoCMake.cmake
+++ cmake/gaaXrepoCMake.cmake
@@ -1,10 +1,4 @@
-set(XREPO_CMAKE "${CMAKE_CURRENT_BINARY_DIR}/xrepo.cmake")
-if(NOT EXISTS ${XREPO_CMAKE})
-    message(STATUS "Downloading xrepo.cmake => ${XREPO_CMAKE}")
-    file(DOWNLOAD "https://raw.githubusercontent.com/xmake-io/xrepo-cmake/main/xrepo.cmake"
-                  "${XREPO_CMAKE}" TLS_VERIFY ON)
-endif()
-include("${XREPO_CMAKE}")
+include(xrepo)
 
 function(gaaFindPackageOrXrepo CMAKE_PACKAGE XREPO_PACKAGE)
     find_package(${CMAKE_PACKAGE} QUIET ${ARGN})
