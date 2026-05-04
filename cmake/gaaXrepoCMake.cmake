ef3407c39ae046b9e27a3b17ea0a48e072068142
WZYivan <2270060975@qq.com>
2026-05-04 13:31:39 +0800

    [build] update xrepo_package invoke

--- cmake/gaaXrepoCMake.cmake
+++ cmake/gaaXrepoCMake.cmake
@@ -5,7 +5,7 @@ function(gaaFindPackageOrXrepo CMAKE_PACKAGE XREPO_PACKAGE)
 
     if(NOT ${CMAKE_PACKAGE}_FOUND)
         message(STATUS "${CMAKE_PACKAGE} not found, installing ${XREPO_PACKAGE} via xrepo...")
-        xrepo_package(${XREPO_CMAKE})
+        xrepo_package("${XREPO_CMAKE}")
         find_package(${CMAKE_PACKAGE} REQUIRED ${ARGN})
     endif()
 endfunction()
