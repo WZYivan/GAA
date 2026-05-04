5dfd38f59328ac75bee98dddbbf5b106a2d8138f
WZYivan <2270060975@qq.com>
2026-05-04 14:38:09 +0800

    [build] add private 3rd deps (1)

---
+++ cmake/gaaPrivate3rd.cmake
@@ -0,0 +1,11 @@
+set(GAA_3RD_DIR "${GAA_ROOT}/3rd/")
+set(GAA_3RD_INCLUDE_DIR "${GAA_3RD_DIR}/include")
+set(GAA_3RD_LIB_DIR "${GAA_3RD_DIR}/lib")
+
+target_include_directories(gaa PUBLIC $<BUILD_INTERFACE:${GAA_3RD_INCLUDE_DIR}>)
+target_include_directories(gaa PUBLIC $<BUILD_INTERFACE:${GAA_3RD_INCLUDE_DIR}/eigen3>)
+
+get_target_property(INC gaa INCLUDE_DIRECTORIES)
+message(STATUS "${INC}")
+
+target_link_libraries(gaa PUBLIC "-L${GAA_3RD_LIB_DIR} -Wl,-rpath=${GAA_3RD_LIB_DIR}")
