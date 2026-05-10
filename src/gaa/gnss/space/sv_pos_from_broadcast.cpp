efe92f7a0e9f0360be317fc31ab69875d8c7f798
WZYivan <2270060975@qq.com>
2026-05-10 23:05:39 +0800

    [chore] solve warnings (1)

--- src/gaa/gnss/space/sv_pos_from_broadcast.cpp
+++ src/gaa/gnss/space/sv_pos_from_broadcast.cpp
@@ -4,9 +4,10 @@ namespace gaa {
 
 std::tuple<double, double, double>
 sv_pos_from_broadcast(double sqrtA, double e, double t_oe, double M_0,
-                      double omega, double i_0, double IDOT, double C_us,
-                      double C_uc, double C_is, double C_ic, double C_rc,
-                      double C_rs, double Omega_0, double OmegaDot, double t) {
+                      double omega, double i_0, double IDOT [[maybe_unused]],
+                      double C_us, double C_uc, double C_is, double C_ic,
+                      double C_rc, double C_rs, double Omega_0, double OmegaDot,
+                      double t) {
   double GM = cgcs2000.gravity().GM(), omegaE = cgcs2000.gravity().omega();
   double n0 = sv_mean_angular_velocity(GM, sqrtA);
 
